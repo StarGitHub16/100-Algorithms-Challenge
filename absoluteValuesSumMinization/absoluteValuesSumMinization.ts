4f028074bdccd4c0d6e64fe03b7cafa62ff909b5
Melad <melad3001@gmail.com>
2023-03-04 01:09:53 -0600

    Started the Algorith Challenge

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,14 @@
+//Try to get the median value( middle number in an array). 
+//The function will need to handle a situation where if there are 2 middle numbers, it returns the least of the 2 aka the one to the left.
+
 export function absoluteValuesSumMinimization(a: number[]): number {
-    return 5;
+
+    const isEven = a.length % 2 === 0;  //First checking to see if the length is even.
+    
+    return isEven ? a[a.length / 2 - 1] : a[Math.floor(a.length/ 2)] //Then if it is is even, we want to take the length divide 2 but subtract 1 to get the middle number that is to the left.
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
