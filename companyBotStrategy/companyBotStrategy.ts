a101daa662436569c0a18862999e6d5466019713
Melad <melad3001@gmail.com>
2023-03-06 17:14:03 -0600

    Algorithms Challenge Part 6

--- companyBotStrategy/companyBotStrategy.ts
+++ companyBotStrategy/companyBotStrategy.ts
@@ -1,7 +1,19 @@
+//In summary the problem is asking what is the average of time and correctness for a right answer.
+
 export function companyBotStrategy(trainingData: number[][]): number {
+   let time = 0;
+   let correctness= 0;
 
-}
+   trainingData.forEach((data: number[]) => {
+        if(data[1] === 1) {
+            time += data[0];
+            correctness == data[1];
+        }
+   });
 
-// console.log(companyBotStrategy([[3, 1], [6, 1], [4, 1], [5, 1]]));
-// console.log(companyBotStrategy([[4, 1], [4, -1], [0, 0], [6, 1]]));
-// console.log(companyBotStrategy( [[4, -1], [0, 0], [5, -1]]));
+   return time / correctness || 0;
+}  
+
+ console.log(companyBotStrategy([[3, 1], [6, 1], [4, 1], [5, 1]]));
+ console.log(companyBotStrategy([[4, 1], [4, -1], [0, 0], [6, 1]]));
+ console.log(companyBotStrategy( [[4, -1], [0, 0], [5, -1]]));
